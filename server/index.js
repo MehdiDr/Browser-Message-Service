78eb54bbd8748b4a6f33e1f191e3a4a4a59c1a29
MehdiDr <mehdidruon@gmail.com>
2017-07-15 21:33:37 +0200

    Add nexmo to send SMS

--- server/index.js
+++ server/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
-const app = express();
-const server = app.listen(3000);
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const path = require('path');
+const Nexmo = require('nexmo');
 
-app.set('views', __dirname + '/../views');
+const app = express();
+const server = app.listen(3000);
+const nexmo = new Nexmo({
+  apiKey: 6834c3b7,
+  apiSecret: bc6ebd41b974275c,
+}, {debug: true});
+
+app.set(path.join('views', __dirname, '/../views'));
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
-app.use(express.static(__dirname + '/../public'))
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/', (req, res) => {
+  res.send(req.body);
+  const toNumber = req.body.number;
+  const text = req.body.text;
+  nexmo.message.sendSms(
+    NUMBER, toNumber text, {type: 'unicode'},
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(response.data)
+      }
+    }
+  );
 });
