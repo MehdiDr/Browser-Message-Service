c8ea756c42a7fb000ce0b119a7552fceab95efce
MehdiDr <mehdidruon@gmail.com>
2017-07-15 23:18:07 +0200

    Add config.js in gitignore and change some css

--- server/config.js
+++ server/config.js
@@ -1,5 +1,10 @@
 module.exports = {
   api_key: '6834c3b7',
   api_secret: 'bc6ebd41b974275c',
-  number: '33782438647'
+  number: '33782438647',
+};
+module.exports = {
+  api_key: '6834c3b7',
+  api_secret: 'bc6ebd41b974275c',
+  number: '33648796093',
 };
